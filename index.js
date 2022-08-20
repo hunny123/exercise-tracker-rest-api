4089b69a60e55553497bb1693e73e9c9841932db
Hunny <devinhunny04021997@gmail.com>
2022-08-20 19:01:10 +0530

    code deploy to heroku

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import * as functions from "firebase-functions";
+
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -6,11 +6,6 @@ import getInstance from "./db/index.js";
 import userRoute from "./routes/user.js";
 import exerciseRoute from "./routes/exercise.js";
 import validProfile from "./services/validProfile.js";
-// import { config } from "dotenv";
-
-// config();
-
-// initial constant and instance
 const PORT = process?.env?.port || 3000;
 const mongo = await getInstance();
 const app = express();
@@ -34,12 +29,3 @@ app.listen(PORT, () => {
   console.log("services is running on" + PORT);
 });
 
-export const apis = functions.https.onRequest(app);
-
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
